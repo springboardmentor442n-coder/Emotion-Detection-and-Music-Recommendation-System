78385e9f7dad8bf973d1adcddf2d7effa6341e63
Sandhiya Mohan Dass <151913129+sandhiyasanthosh@users.noreply.github.com>
2026-03-05 18:26:47 +0530

    Milestone 1: Add emotion to music mapping

---
+++ datasets/preprocessing/emotion_music_mapping.py
@@ -0,0 +1,10 @@
+emotion_music = {
+    "happy": ["pop", "dance"],
+    "sad": ["acoustic", "slow"],
+    "angry": ["rock", "metal"],
+    "neutral": ["lofi", "instrumental"],
+    "surprise": ["electronic"],
+    "fear": ["ambient"]
+}
+
+print("Emotion to music mapping created")
